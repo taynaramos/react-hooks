c26903f4d24941c7c2b6308a86a069910fc41409
Tayná Martins Ramos <tayna.ramos@ifood.com.br>
2026-04-19 11:52:47 -0300

    create useLocalStorageState hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,38 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+// Para criar um custom hook (função que usa react hooks dentro dela deve iniciar com "use" por conveção
+const useLocalStorageState = (
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) => {
+  // usando useState Lazy Initialization o localStorage so vai ser consultado uma vez (na primeira montagem), ele ignora completamente a função () => getInitialName(initialName) a cada renderização do useEffect. Se não usasse essa abordage, o localStorage (que é lento) seria consultado toda vez que que o componente montasse novamente pq do useEffect
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage);
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  // Cada vez que você usa o hook useLocalStorageState em um componente diferente, o React cria um "espaço na memória" totalmente novo e independente para aquele componente. Logo não tem perigo de acessar prevKey de diferentes componentes.
+  const prevKeyRef = React.useRef(key);
+
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeIem(prevKey) // para não guardar lixo no navegador
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState("name", initialName)
 
   function handleChange(event) {
     setName(event.target.value)
