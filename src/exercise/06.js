f05381ef48f5630aa8bafa52dec53eea42d48582
Tayná Martins Ramos <tayna.ramos@ifood.com.br>
2026-05-01 12:50:46 -0300

    use resetKeys

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -14,26 +14,26 @@ import {
   PokemonInfoFallback,
 } from '../pokemon'
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {hasError: false}
-  }
+// class ErrorBoundary extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {hasError: false}
+//   }
 
-  static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
-    return {hasError: true}
-  }
+//   static getDerivedStateFromError(error) {
+//     // Update state so the next render will show the fallback UI.
+//     return {hasError: true}
+//   }
 
-  render() {
-    if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return this.props.fallback
-    }
+//   render() {
+//     if (this.state.hasError) {
+//       // You can render any custom fallback UI
+//       return this.props.fallback
+//     }
 
-    return this.props.children
-  }
-}
+//     return this.props.children
+//   }
+// }
 
 const REQ_STATUS = {
   IDLE: 'idle',
@@ -111,7 +111,7 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonErrorBoundary key={pokemonName}>
+        <PokemonErrorBoundary resetKeys={[pokemonName]}>
           <PokemonInfo pokemonName={pokemonName} />
         </PokemonErrorBoundary>
       </div>
